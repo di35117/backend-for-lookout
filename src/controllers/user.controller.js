0ebba0bad52be3571a59d25b9e998b37f8984e01
dishank choudhury <dishankchoudhury@gmail.com>
2025-09-09 19:05:39 +0530

    leaderboard added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import { User } from "../models/user.model.js";
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, req.user, "User profile fetched successfully"));
 });
+const getLeaderboard = asyncHandler(async (req, res) => {
+  const leaderboard = await User.find({})
+    .sort({ score: -1 })
+    .limit(7)
+    .select("username score");
 
-export { getCurrentUser };
+  if (!leaderboard) {
+    throw new ApiError(404, "Leaderboard data not found");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, leaderboard, "Top 7 players fetched successfully")
+    );
+});
+export { getCurrentUser, getLeaderboard };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { getCurrentUser } from "../controllers/user.controller.js";
+import {
+  getCurrentUser,
+  getLeaderboard,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
+router.route("/leaderboard").get(getLeaderboard);
 router.route("/profile").get(verifyJWT, getCurrentUser);
+
 export default router;
